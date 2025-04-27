eb6dbca979b8f19c84d5f044558e06e80d5ffc77
avi-1227 <avinaashprasadd@gmail.com>
2025-04-27 23:43:05 +0530

    Add variable content

---
+++ basics/01-variables/hello_world.py
@@ -0,0 +1,4 @@
+# Variables :- Variables are often described as boxes you can store values in. Much better to think of variables as lables that you can assign to values
+
+message = "Hello Python World!"
+print(message)
