f978a54fbd90472c04b7f303d6f81416767503d2
avi-1227 <avinaashprasadd@gmail.com>
2025-04-27 23:44:06 +0530

    Create content about String Data type

---
+++ basics/02-data-types/strings/name.py
@@ -0,0 +1,9 @@
+# Strings - A string is a series of characters. Anything inside quotes is considered a string in Python and you can use single or double quotes around your strings like this:
+# eg - "This is a string" or 'This is also a string'
+# This flexibility allows you to use quotes and apostrophes within your strings
+
+# Changing Case in a String with Methods
+# title() method changes each work to title case, where each word begins with a capital letter
+
+name = "ada lovelace"
+print(name.title())
