705fc56f66f9132253d57077ca88c6a9f762fd3a
avi-1227 <avinaashprasadd@gmail.com>
2025-04-28 13:35:28 +0530

    Add float number

---
+++ basics/02-data-types/02-numbers/b_float_number.py
@@ -0,0 +1,18 @@
+# Float - Python calls any number with a decimal point a float.
+
+number_1 = 20.50
+number_2 = 5.10
+add_numbers = number_1 + number_2
+substract_numbers = number_1 - number_2
+multiply_numbers = number_1 * number_2
+divide_numbers = number_1 / number_2
+print(add_numbers)
+print(substract_numbers)
+print(multiply_numbers)
+print(divide_numbers)
+
+# Integers and Floats
+# When you divide any two numbers, even if they are integers that result in a whole number, you'll always get a float.
+# eg. 4/2 = 2.0
+# If you mix an integer and a float in any other operation, you'll get a float as well:
+# eg. 1+2.0 = 3.0, 2*3.0 = 6.0, 3.0 ** 2 = 9.0
