12315162ba009640483f924a5653aeae3093006a
avi-1227 <avinaashprasadd@gmail.com>
2025-04-28 13:17:16 +0530

    Add Integer Number

---
+++ basics/02-data-types/numbers/integer_number.py
@@ -0,0 +1,15 @@
+# Numbers - Numbers are used quite often in programming to keep score in games, represent data in visualizations, store information in web applications, and so on.
+
+# Integers - You can add, subtract, multiply and divide integers in Python
+number_1 = 20
+number_2 = 5
+add_numbers = number_1 + number_2
+substract_numbers = number_1 - number_2
+multiply_numbers = number_1 * number_2
+divide_numbers = number_1 / number_2
+print(add_numbers)
+print(substract_numbers)
+print(multiply_numbers)
+print(divide_numbers)
+
+
