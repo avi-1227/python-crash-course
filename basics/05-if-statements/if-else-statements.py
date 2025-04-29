ceb75f01fd0f907c1d8b73b37937441f48d30971
avi-1227 <avinaashprasadd@gmail.com>
2025-04-29 17:43:48 +0530

    Add if else content

---
+++ basics/05-if-statements/if-else-statements.py
@@ -0,0 +1,10 @@
+# if-else statement
+'''
+Often, you'll want to take one action when a conditional test passes and a different action in all other cases. Python's if-else syntax makes this possible.
+An if-else block is similar to a simple if statement, but the else statement allows you to define an action or set of actions that are executed when the conditional test fails.
+'''
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+else:
+    print("Sorry you are too young to vote.")
