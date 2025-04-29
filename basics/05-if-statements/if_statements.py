3ca683613e3e711c9131644a8a985f3c3ba7b424
avi-1227 <avinaashprasadd@gmail.com>
2025-04-29 16:47:02 +0530

    Add if statement content

---
+++ basics/05-if-statements/if_statements.py
@@ -0,0 +1,10 @@
+# Simple if statements
+''' The simplest kind of if statement has one test and one action.
+
+syntax:
+    if conditional test:
+        do something
+'''
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
